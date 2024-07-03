f90cda038f287695fd5a3f224018f645e69becdc
VishAyush <139580532+INFERNO1211@users.noreply.github.com>
2024-07-03 19:48:15 +0530

    added icons and logo

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -20,10 +20,9 @@ const Intro = () => {
     <div className='flex justify-center items-center min-h-[720px]'>
     <div className='shadowy rounded-md p-20 text-white intro w-[60%] bg-gradient-to-br from-slate-900 to-slate-700'>
         <div className='pb-8'><h1 className='text-5xl'>Hi, I'm <span ref={el} className="auto-input"></span>,</h1></div>
-        <div className='pb-8 text-xl'><p>I am a skilled frontend and blockchain developer with expertise in Solidity, React, and full stack development. I excel in creating dynamic web applications and secure smart contracts, seamlessly integrating blockchain technology.
-
-</p></div>
+        <div className='pb-8 text-xl'><p>I am a skilled frontend and blockchain developer with expertise in Solidity, React, and full stack development. I excel in creating dynamic web applications and secure smart contracts, seamlessly integrating blockchain technology.</p></div>
         <div className='text-xl pb-8'><p> With a strong foundation in data structures and algorithms, I am dedicated to building innovative, user-friendly solutions. Passionate about the revolutionary potential of technology, I strive to push the boundaries of what's possible in the digital world.</p></div>
+        <div className='text-center mt-4'><button type='button' className='bg-white text-black p-3 rounded-sm font-bold shadowy hover:bg-gray-500 hover:text-white duration-200'>Resume</button></div>
     </div>
 </div>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
-
+import logo from '../assets/logo.png';
 const Navbar = () => {
 
 
   return (
+    <>
+    <img className="w-16 fixed mt-4 ml-10" src={logo}  alt="" />
     <nav className='py-6 mt-3 fixed flex text-white w-1/3 h-10 rounded-sm justify-around items-center left-1/2 transform -translate-x-1/2' >
         <div className='links cursor-pointer p-2' >About Me</div>
         <div className='links cursor-pointer p-2 ' >Projects</div>
         <div className='links cursor-pointer p-2 ' >Contact Me</div>
     </nav>
+    </>
   )
 }
 
